cfc26052e782b63c7d8d56f1aff78149fd3be68d
SSANGWA2 <ssangwa2@gmail.com>
2025-03-08 21:02:37 +0530

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,36 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-//conflict case2 local
-	conflictlocal2(){
-	}
-
-
-
-//conflict case2 master
-	conflictmaste(){
-	}
-
-//conflict case1 local
-	conflictlocal(){
-	}
-
-
-//conflict case1 remote
-	conflict(){
-	}
-
-
-//local to remote-1
-hello buddy1(){
-
-}
-	//remote to local-2
-hello buddy2(){
-
-}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
