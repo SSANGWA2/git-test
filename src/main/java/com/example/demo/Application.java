c8265be560e940315fdc6fb85b1a7700e3c8db5c
suresh <ssangwa2@gmail.com>
2025-03-08 17:56:13 +0530

    firt edit from local to remote

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+
+//local to remote-1
+hello buddy(){
+
+}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
